7928b34ee4dd0c7cbd5433e76556656c45ef75d3
mgazizova <mgazizova@naumen.ru>
2019-12-22 14:49:09 +0500

    add controller for testcase

---
+++ src/main/java/hello/TestCaseRepository.java
@@ -0,0 +1,10 @@
+package hello;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TestCaseRepository extends JpaRepository<TestCase, String> {
+
+}
